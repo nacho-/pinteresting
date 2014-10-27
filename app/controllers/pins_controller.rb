38e0d6b12fd7112eff675f3866d2389e45e527b9
Ignacio Alonso <nacho.allibre@gmail.com>
2014-10-27 02:16:13 -0600

    filters and permissions

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,8 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
-
- # 7 main actions
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @pins = Pin.all
   end
@@ -10,54 +9,50 @@ class PinsController < ApplicationController
   def show
   end
 
- 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
-  
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-    
-      else
-        format.html { render :new }
-
-        end
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
- 
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-       else
-        format.html { render :edit }
-          end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      end
+    redirect_to pins_url
   end
 
-  #private controllers 
   private
-  def set_pin
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
       @pin = Pin.find(params[:id])
     end
 
-     def pin_params
-      params.require(:pin).permit(:description)
+
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description, :image)
     end
 end
